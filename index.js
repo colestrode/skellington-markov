317fdb6e99355ab816e1cd10b1b9cf42e077e3b6
Cole Furfaro-Strode <colestrode@gmail.com>
2016-03-20 23:00:40 -0400

    removes polling of user list

--- index.js
+++ index.js
@@ -12,6 +12,4 @@ plugin.init = function(controller, bot) {
   controller.on('channel_joined', listeners.joined);
   controller.on('group_joined', listeners.joined);
   controller.hears('what do you (think|say) about (.*)', 'direct_message,direct_mention,mention', listeners.markovRespond);
-
-  setInterval(userCache.populateCache.bind(userCache), 5*60*1000); // update user cache so that it won't get stale
 };
