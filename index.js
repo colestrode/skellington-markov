083d655b1cc12d609f3b3ca18d4e428a89d713c2
Cole Furfaro-Strode <colestrode@gmail.com>
2016-03-21 15:45:32 -0400

    uses channel history instead of ambient listener

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ let userCache = require('./lib/userCache');
 let plugin = module.exports;
 
 plugin.init = function(controller, bot) {
-  listeners.init(controller);
   userCache.init(bot);
 
-  controller.on('ambient', listeners.ambient);
   controller.on('channel_joined', listeners.joined);
   controller.on('group_joined', listeners.joined);
   controller.hears('what do you (think|say) about (.*)', 'direct_message,direct_mention,mention', listeners.markovRespond);
