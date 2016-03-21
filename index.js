620e95f3db1f326ddaade938b346f721fea02665
Cole Furfaro-Strode <colestrode@gmail.com>
2016-03-20 22:43:37 -0400

    adds filtering for usernames and user ids

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 
 let listeners = require('./lib/listeners');
+let userCache = require('./lib/userCache');
 let plugin = module.exports;
 
-plugin.init = function(controller) {
+plugin.init = function(controller, bot) {
   listeners.init(controller);
+  userCache.init(bot);
+
   controller.on('ambient', listeners.ambient);
-  controller.hears('what do you (think|say)', 'direct_message,direct_mention,mention', listeners.markovRespond);
+  controller.on('channel_joined', listeners.joined);
+  controller.on('group_joined', listeners.joined);
+  controller.hears('what do you (think|say) about (.*)', 'direct_message,direct_mention,mention', listeners.markovRespond);
+
+  setInterval(userCache.populateCache.bind(userCache), 5*60*1000); // update user cache so that it won't get stale
 };
